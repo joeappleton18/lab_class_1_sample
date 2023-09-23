4a1b9bb4d34a107f9630c8051875cb7db7876366
joeappleton18 <joeappleton18@gmail.com>
2023-09-23 11:23:24 +0100

    final tweaks done

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const mysql = require('mysql');
 const util = require('util');
-const e = require('express');
+require('express');
 const port = 8000;
 
 const app = express();
